de1f89e7ac40a55accc788e7104581e01cd74523
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-12-10 09:37:14 -0300

    pdo

--- Banco_de_dados/Php_PDO/index.php
+++ Banco_de_dados/Php_PDO/index.php
@@ -1,20 +1,25 @@
 <?php 
-   $dsn = 'mysql:host=localhost;dbname=banco_teste';
-   $user = 'root';
-   $pass = '';
 
-   try{
-        $link = new PDO($dsn, $user, $pass);
-   }catch(PDOException $e) {
-        echo 'Erro'.$e->getCode().'Mensagem'.$e->getMessage();
+   if (!empty($_POST['usuario']) && !empty($_POST['senha'])) {
+        $dsn = 'mysql:host=localhost;dbname=banco_teste';
+        $user = 'root';
+        $pass = '';
+
+        try{
+            $link = new PDO($dsn, $user, $pass);
+
+            $pergunta = "select * from tb_usuario where";
+            $pergunta.= " usuario = '{$_POST['usuario']}'";
+            $pergunta.= " and senha = '{$_POST['senha']}'";
+
+            $resposta = $link->query($pergunta);
+            $usuario = $resposta->fetch();
+            print_r($usuario);
+
+        }catch(PDOException $e) {
+            echo 'Erro'.$e->getCode().'Mensagem'.$e->getMessage();
+        }
    }
-
-   $pergunta = 'SELECT * FROM tb_usuario';
-   $resultado = $link->query($pergunta);
-   $lista = $resultado->fetchAll(PDO::FETCH_ASSOC);
-   //echo '<pre>';
-   //print_r($lista);
-   //echo '</pre>';
 ?>
 
 <!DOCTYPE html>
@@ -27,9 +32,48 @@
 <body>
     <form action="index.php" method="post">
         <h1>Login</h1>
-        <input type="text" name="login" placeholder="Login"> <br>
+        <input type="text" name="usuario" placeholder="Usuario"> <br>
         <input type="password" name="senha" placeholder="Senha"><br>
         <button type="submit">Entrar</button>
     </form>
+    <style>
+        body{
+            width: 100%;
+            height: 100vh;
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            background-color: #EEEEEE;
+            font-family: Arial;
+        }
+
+        form{
+            width: 30%;
+            border-radius: 10px;
+            background-color: #FFFFFF;
+            box-shadow: 0px 10px 15px 0px rgba(0,0,0,0.2);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+
+        h1{
+            font-weight: 400;
+        }
+
+        input{
+            width: 70%;
+            height: 5vh;
+        }
+
+        button{
+            width: 70%;
+            height: 40px;
+            margin-bottom: 30px;
+        }
+    </style>
 </body>
 </html>
